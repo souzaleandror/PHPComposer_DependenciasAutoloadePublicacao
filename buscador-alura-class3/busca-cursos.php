0242a734cf9e1f16cbfb63bf57f1ff048acc7d76
Leandro Rodrigues Souza <souza.leandror@gmail.com>
2024-02-03 21:54:53 +0100

    Class 03 - Understanding Autoload done

--- buscador-alura-class3/busca-cursos.php
+++ buscador-alura-class3/busca-cursos.php
@@ -12,5 +12,5 @@ $buscador = new Buscador($client, $crawler);
 $cursos = $buscador->buscar('/cursos-online-programacao/php');
 
 foreach ($cursos as $curso) {
-    echo $curso . PHP_EOL;
+    echo exibeMensagem($curso);
 }
